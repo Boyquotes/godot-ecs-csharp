be73192c55d50feaf3fa9cbd5dc7b98d7e264ac7
CodeNMore <jackofall55@gmail.com>
2023-02-11 15:54:00 -0500

    Added Log to namespace

--- addons/godot-ecs-csharp/util/Log.cs
+++ addons/godot-ecs-csharp/util/Log.cs
@@ -1,47 +1,52 @@
 using System;
 using Godot;
 
-public sealed class Log
+namespace GdEcs
 {
 
-    public const int LOG_LEVEL_ALL = 0,
-                        LOG_LEVEL_DEBUG = 10,
-                        LOG_LEVEL_INFO = 20,
-                        LOG_LEVEL_WARN = 30,
-                        LOG_LEVEL_ERR = 40,
-                        LOG_LEVEL_NONE = 50;
-
-    public static int LogLevel = LOG_LEVEL_WARN;
-
-    private Log() { }
-
-    public static void Debug(string msg, Exception? exception = null)
+    public sealed class Log
     {
-        LogPrefixed(LOG_LEVEL_DEBUG, "DEBUG", msg, exception);
-    }
 
-    public static void Info(string msg, Exception? exception = null)
-    {
-        LogPrefixed(LOG_LEVEL_INFO, "INFO", msg, exception);
-    }
+        public const int LOG_LEVEL_ALL = 0,
+                            LOG_LEVEL_DEBUG = 10,
+                            LOG_LEVEL_INFO = 20,
+                            LOG_LEVEL_WARN = 30,
+                            LOG_LEVEL_ERR = 40,
+                            LOG_LEVEL_NONE = 50;
 
-    public static void Warn(string msg, Exception? exception = null)
-    {
-        LogPrefixed(LOG_LEVEL_WARN, "WARN", msg, exception);
-    }
+        public static int LogLevel = LOG_LEVEL_WARN;
 
-    public static void Error(string msg, Exception? exception = null)
-    {
-        LogPrefixed(LOG_LEVEL_ERR, "ERROR", msg, exception);
-    }
+        private Log() { }
 
-    private static void LogPrefixed(int level, string prefix, string msg, Exception? exception)
-    {
-        if (level < LogLevel)
-            return;
-        GD.Print($"{DateTime.Now} [{prefix}] {msg}");
-        if (exception != null)
-            GD.PrintErr($"\tException: {exception.Message}\n{exception.StackTrace}");
+        public static void Debug(string msg, Exception? exception = null)
+        {
+            LogPrefixed(LOG_LEVEL_DEBUG, "DEBUG", msg, exception);
+        }
+
+        public static void Info(string msg, Exception? exception = null)
+        {
+            LogPrefixed(LOG_LEVEL_INFO, "INFO", msg, exception);
+        }
+
+        public static void Warn(string msg, Exception? exception = null)
+        {
+            LogPrefixed(LOG_LEVEL_WARN, "WARN", msg, exception);
+        }
+
+        public static void Error(string msg, Exception? exception = null)
+        {
+            LogPrefixed(LOG_LEVEL_ERR, "ERROR", msg, exception);
+        }
+
+        private static void LogPrefixed(int level, string prefix, string msg, Exception? exception)
+        {
+            if (level < LogLevel)
+                return;
+            GD.Print($"{DateTime.Now} [{prefix}] {msg}");
+            if (exception != null)
+                GD.PrintErr($"\tException: {exception.Message}\n{exception.StackTrace}");
+        }
+
     }
 
 }
