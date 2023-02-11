c6996a84695eff637c374fc7354d3c3597b77ec1
CodeNMore <jackofall55@gmail.com>
2023-02-11 16:26:33 -0500

    Snap fix

--- addons/godot-ecs-csharp/components/RayCastSnapComponent.cs
+++ addons/godot-ecs-csharp/components/RayCastSnapComponent.cs
@@ -3,22 +3,12 @@ using Godot;
 namespace GdEcs
 {
 
-    [Tool]
     [ExportCustomNode("component", "RayCast")]
     public class RayCastSnapComponent : RayCastComponent
     {
 
         [Export]
         public bool OneShot { get; set; } = true;
-
-        public override void _EnterTree()
-        {
-            base._EnterTree();
-            if (!Engine.EditorHint)
-                return;
-            if (CastTo == new Vector3(0, -1, 0))
-                CastTo = new Vector3(0, -10000, 0);
-        }
 
     }
 
