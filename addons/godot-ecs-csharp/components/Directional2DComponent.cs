5ebdda755cc9ba02f776786c37a8ed836e2dc87c
CodeNMore <jackofall55@gmail.com>
2023-02-11 23:01:18 -0500

    Add direction

--- addons/godot-ecs-csharp/components/Directional2DComponent.cs
+++ addons/godot-ecs-csharp/components/Directional2DComponent.cs
@@ -12,6 +12,40 @@ namespace GdEcs
 
         public Vector2 DirVecNormalized => RawDirVec.Normalized();
 
+        public Direction Direction
+        {
+            get
+            {
+                if (RawDirVec.y < 0)
+                {
+                    if (RawDirVec.x < 0)
+                        return Direction.UpLeft;
+                    else if (RawDirVec.x > 0)
+                        return Direction.UpRight;
+                    else
+                        return Direction.Up;
+                }
+                else if (RawDirVec.y > 0)
+                {
+                    if (RawDirVec.x < 0)
+                        return Direction.DownLeft;
+                    else if (RawDirVec.x > 0)
+                        return Direction.DownRight;
+                    else
+                        return Direction.Down;
+                }
+                else
+                {
+                    if (RawDirVec.x < 0)
+                        return Direction.Left;
+                    else if (RawDirVec.x > 0)
+                        return Direction.Right;
+                    else
+                        return Direction.None;
+                }
+            }
+        }
+
     }
 
 }
---
+++ addons/godot-ecs-csharp/util/Direction.cs
@@ -0,0 +1,17 @@
+namespace GdEcs
+{
+
+    public enum Direction
+    {
+        None,
+        Left,
+        Right,
+        Up,
+        Down,
+        UpLeft,
+        UpRight,
+        DownLeft,
+        DownRight,
+    }
+
+}
